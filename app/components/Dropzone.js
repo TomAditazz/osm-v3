abb7f6a19f1588f1ad9e2aada837316a14ac80e4
kYoTang <155143448@qq.com>
2016-11-17 13:01:05 -0800

    edit using ol3

--- app/components/Dropzone.js
+++ app/components/Dropzone.js
@@ -7,16 +7,33 @@ var DropzoneDemo = React.createClass({
       console.log('Accepted files: ', acceptedFiles[0].preview);
       console.log('Rejected files: ', rejectedFiles);
             //Initialise the vector layer using OpenLayers.Format.OSM
-      layer = new OpenLayers.Layer.Vector("Polygon", {
-          strategies: [new OpenLayers.Strategy.Fixed()],
-          protocol: new OpenLayers.Protocol.HTTP({
-              url: acceptedFiles[0].preview,   //<-- relative or absolute URL to your .osm file
-              format: new OpenLayers.Format.OSM()
-          }),
-          projection: new OpenLayers.Projection("EPSG:4326")
+      var lat=50.88;
+      var lon=-1.54;
+      var zoom=13;
+      var vectorSource = new ol.source.Vector({
+        url: acceptedFiles[0].preview,
+        format: new ol.format.OSMXML()
       });
-      console.log(layer);
-      map.addLayers([layer]);
+
+
+      map = new ol.Map({
+        layers: [
+          new ol.layer.Tile({
+            source: new ol.source.OSM(),
+          }),
+          new ol.layer.Vector({
+            source: vectorSource,
+            projection: 'EPSG:3857'
+          })
+        ],
+        target: 'map',
+        view: new ol.View({
+          projection: 'EPSG:3857',
+          center: ol.proj.transform([lon, lat], 'EPSG:4326', 'EPSG:3857'),
+          maxZoom: 19,
+          zoom: 13
+        })
+      });
     },
 
     render: function () {
--- app/components/OsmEditer.js
+++ app/components/OsmEditer.js
@@ -8,9 +8,8 @@ var OsmEditer = React.createClass({
     componentWillMount() {
       const script = document.createElement("script");
 
-        script.src = "http://openlayers.org/api/OpenLayers.js";
+        script.src = "https://openlayers.org/en/v3.19.1/build/ol.js";
         //script.async = true;
-
         document.body.appendChild(script);
         console.log(script);
     },
@@ -18,121 +17,68 @@ var OsmEditer = React.createClass({
 
     },
     initialMap(){
-      var lat=50.88;
-      var lon=-1.54;
-      var zoom=13;
 
-      map = new OpenLayers.Map ("map", {
-        controls:[
-            new OpenLayers.Control.Navigation(),
-            new OpenLayers.Control.PanZoomBar(),
-            new OpenLayers.Control.LayerSwitcher(),
-            new OpenLayers.Control.Attribution()],
-        maxExtent: new OpenLayers.Bounds(-20037508.34,-20037508.34,20037508.34,20037508.34),
-        maxResolution: 156543.0399,
-        numZoomLevels: 19,
-        units: 'm',
-        projection: new OpenLayers.Projection("EPSG:900913"),
-        displayProjection: new OpenLayers.Projection("EPSG:4326")
-      });
-      map.addLayer(new OpenLayers.Layer.OSM());
- 
-      var lonLat = new OpenLayers.LonLat(lon, lat).transform(new OpenLayers.Projection("EPSG:4326"), new OpenLayers.Projection("EPSG:900913"));
-      
-      map.setCenter (lonLat, zoom);
     },
 
     updateLayers(){
-      //Initialise the vector layer using OpenLayers.Format.OSM
-      var layer = new OpenLayers.Layer.Vector("Polygon", {
-          strategies: [new OpenLayers.Strategy.Fixed()],
-          protocol: new OpenLayers.Protocol.HTTP({
-              url: "test.osm",   //<-- relative or absolute URL to your .osm file
-              format: new OpenLayers.Format.OSM()
-          }),
-          projection: new OpenLayers.Projection("EPSG:4326")
-      });
-      map.addLayers([layer]);
+
     },
 
     editMap(){
-      layer.destroy();
-      vlayer = new OpenLayers.Layer.Vector( "Editable", {
-          strategies: [new OpenLayers.Strategy.Fixed()],
-          protocol: new OpenLayers.Protocol.HTTP({
-              url: "test.osm",   //<-- relative or absolute URL to your .osm file
-              format: new OpenLayers.Format.OSM()
-          }),
-          projection: new OpenLayers.Projection("EPSG:4326")
+      var editorSource = new ol.source.Vector({
+        url: "test.osm",
+        format: new ol.format.OSMXML()
       });
-      map.addLayer(vlayer);
-      zb = new OpenLayers.Control.ZoomBox({
-          title: "Zoom box: zoom clicking and dragging",
-          text: "Zoom"
-      });
+     
+      var editlayer = new ol.layer.Vector({
+        source: editorSource,
+        projection: 'EPSG:3857'
+      })
+      map.addLayer(editlayer);
+      var typeSelect = document.getElementById('type');
 
-      var panel = new OpenLayers.Control.Panel({
-          defaultControl: zb,
-          createControlMarkup: function(control) {
-              var button = document.createElement('button'),
-                  iconSpan = document.createElement('span'),
-                  textSpan = document.createElement('span');
-              iconSpan.innerHTML = '&nbsp;';
-              button.appendChild(iconSpan);
-              if (control.text) {
-                  textSpan.innerHTML = control.text;
-              }
-              button.appendChild(textSpan);
-              return button;
-          }
-      });
+      var draw; // global so we can remove it later
+      function addInteraction() {
+        var value = typeSelect.value;
+        if (value !== 'None') {
+          draw = new ol.interaction.Draw({
+            source: editorSource,
+            type: /** @type {ol.geom.GeometryType} */ (typeSelect.value)
+          });
+          map.addInteraction(draw);
+        }
+      }
 
-      panel.addControls([
-          zb,
-          new OpenLayers.Control.DrawFeature(vlayer, OpenLayers.Handler.Path,
-              {title:'Draw line', text: 'Line'}),
-          new OpenLayers.Control.DrawFeature(vlayer, OpenLayers.Handler.Polygon,
-              {title:'Draw Polygon', text: 'Polygon'}),
-          //new OpenLayers.Control.ZoomToMaxExtent({
-          //    title:"Zoom to the max extent",
-          //    text: "World"
-          //}),
-          //new OpenLayers.Control.DrawFeature.cancle(),
-      ]);
-      
-      nav = new OpenLayers.Control.NavigationHistory({
-          previousOptions: {
-              title: "Go to previous map position",
-              text: "Prev"
-          },
-          nextOptions: {
-              title: "Go to next map position",
-              text: "Next"
-          },
-          displayClass: "navHistory"
-      });
-      // parent control must be added to the map
-      map.addControl(nav);
-      panel.addControls([nav.next, nav.previous]);
-      
-      map.addControl(panel);
+
+      /**
+       * Handle change event.
+       */
+      typeSelect.onchange = function() {
+        map.removeInteraction(draw);
+        addInteraction();
+      };
+
+      addInteraction();
     },
 
   render: function(){
-    var options={
-        baseUrl:'http://127.0.0.1:3000/',
-        param:{
-            fid:0
-        }
-    }
     return (
       <div className="col-sm-6">
         <div className="col-sm-12">
           <Dropzonedemo />
-          <div id="map">
-            <button type="button" onClick={this.initialMap}>Initial Map</button>
-            <button type="button" onClick={this.editMap}>Edit Map</button>
-            <button type="button">Output Map</button>
+          <div id="map" class="map">
+            <button type="button" onClick={this.editMap}>Edit Map</button>  
+            <form class="form-inline">
+              <label>Geometry type &nbsp;</label>
+              <select id="type">
+                <option value="Point">Point</option>
+                <option value="LineString">LineString</option>
+                <option value="Polygon">Polygon</option>
+                <option value="Circle">Circle</option>
+                <option value="None">None</option>
+              </select>
+              <button type="button">Output Map</button>
+            </form>
           </div>
         </div>
       </div>
